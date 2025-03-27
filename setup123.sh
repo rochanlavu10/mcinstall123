643fc504e290feeadd8ae59e2c1fc2159a50647a
AMBIGIO <76466669+rochanlavu10@users.noreply.github.com>
2025-03-27 08:26:18 +0800

    Update setup123.sh

--- setup123.sh
+++ setup123.sh
@@ -3,7 +3,9 @@ sudo apt upgrade -y
 sudo apt install openjdk-17-jre -y
 echo OpenJdk 17 has been installed!
 mkdir ~/.minecraft
-wget -P ~/.minecraft https://llaun.ch/jar
-mv ~/.minecraft/jar ~/.minecraft/tlaunch.jar
-java -jar ~/.minecraft/tlaunch.jar | awk '/[Thread-0] INFO shaded.com.getsentry.raven.connection.AsyncConnection - Shutdown finished./ {system("^C")}'
-
+wget https://llaun.ch/jar
+mv jar tlaunch.jar
+mkdir ~/.local/share/applications
+wget -P ~/.local/share/applications https://raw.githubusercontent.com/rochanlavu10/mcinstall123/refs/heads/main/minecraft.desktop
+java -jar tlaunch.jar | awk '/[Thread-0] INFO shaded.com.getsentry.raven.connection.AsyncConnection - Shutdown finished./ {system("^C")}'
+echo Minecraft launcher (tlauncher legacy) has been installed!
