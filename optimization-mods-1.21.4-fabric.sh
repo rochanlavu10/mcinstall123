6b0a95cd75ccace5ebb1808bf9989b8be8fe1762
AMBIGIO <76466669+rochanlavu10@users.noreply.github.com>
2025-03-28 09:49:55 +0800

    Add files via upload

---
+++ optimization-mods-1.21.4-fabric.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rochanlavu10/mcinstall123/raw/refs/heads/main/mods.zip
+unzip mods.zip
+mv mods/ .minecraft/mods
+echo Optimization mods for Minecraft 1.21.4 Fabric have been installed!
