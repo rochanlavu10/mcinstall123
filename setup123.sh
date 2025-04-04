18e68734f9fd6af9a46f5140e96cd9156ef1e6ab
AMBIGIO <76466669+rochanlavu10@users.noreply.github.com>
2025-04-04 09:27:17 +0800

    Update setup123.sh

--- setup123.sh
+++ setup123.sh
@@ -6,5 +6,6 @@ wget https://llaun.ch/jar
 mv jar tlaunch.jar
 mkdir ~/.local/share/applications
 wget -P ~/.local/share/applications https://raw.githubusercontent.com/rochanlavu10/mcinstall123/refs/heads/main/minecraft.desktop
+wget -P ~/.tlauncher https://raw.githubusercontent.com/rochanlavu10/mcinstall123/refs/heads/main/legacy.properties
 java -jar tlaunch.jar | awk '/[Thread-0] INFO shaded.com.getsentry.raven.connection.AsyncConnection - Shutdown finished./ {system("^C")}'
 echo Minecraft launcher (tlauncher legacy) has been installed! You can now click "Minecraft" in the application launcher to open it
