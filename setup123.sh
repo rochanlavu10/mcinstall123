09b1e5b99ccf7d28ce827c6533e1ab4c3aa686ee
Lavu Rochan Reddy <rochan.lavu@gmail.com>
2026-01-13 19:22:51 +0800

    Update setup123.sh

--- setup123.sh
+++ setup123.sh
@@ -7,5 +7,5 @@ mv jar tlaunch.jar
 mkdir ~/.local/share/applications
 wget -P ~/.local/share/applications https://raw.githubusercontent.com/rochanlavu10/mcinstall123/refs/heads/main/minecraft.desktop
 wget -P ~/.tlauncher https://raw.githubusercontent.com/rochanlavu10/mcinstall123/refs/heads/main/legacy.properties
-java -jar tlaunch.jar
 xmessage "Minecraft launcher (tlauncher legacy) has been installed! You can now click "Minecraft" in the application launcher to open it"
+java -jar tlaunch.jar
