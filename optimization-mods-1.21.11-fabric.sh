825b56beebfc6331887d0d11ca6c060d40528001
Lavu Rochan Reddy <rochan.lavu@gmail.com>
2026-01-13 19:25:30 +0800

    Update and rename optimization-mods-1.21.4-fabric.sh to optimization-mods-1.21.11-fabric.sh

---
+++ optimization-mods-1.21.11-fabric.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rochanlavu10/mcinstall123/raw/refs/heads/main/mods.zip
+unzip mods.zip
+mv mods/ .minecraft/home/"Fabric 1.21.11"
+xmessage "Optimization mods for Minecraft 1.21.11 Fabric have been installed!"
