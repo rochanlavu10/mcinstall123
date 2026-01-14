762d4aff4247d5b9785c21e7d9666d07b8d5d7b8
Lavu Rochan Reddy <rochan.lavu@gmail.com>
2026-01-14 11:27:47 +0800

    Fix mod directory structure for Minecraft installation

--- optimization-mods-1.21.11-fabric.sh
+++ optimization-mods-1.21.11-fabric.sh
@@ -1,5 +1,5 @@
 wget https://github.com/rochanlavu10/mcinstall123/raw/refs/heads/main/mods.zip
 unzip mods.zip
 mkdir .minecraft/home
-mv mods .minecraft/home/"Fabric 1.21.11"
+mv mods/ .minecraft/home/"Fabric 1.21.11"/mods
 xmessage "Optimization mods for Minecraft 1.21.11 Fabric have been installed!"
