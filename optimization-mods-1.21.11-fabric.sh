a26efd31ff46514904711ccca0f4373128053ef2
Lavu Rochan Reddy <rochan.lavu@gmail.com>
2026-01-14 19:47:21 +0800

    Update optimization-mods-1.21.11-fabric.sh

--- optimization-mods-1.21.11-fabric.sh
+++ optimization-mods-1.21.11-fabric.sh
@@ -1,6 +1,5 @@
-wget https://github.com/rochanlavu10/mcinstall123/raw/refs/heads/main/mods.zip
-unzip mods.zip
+wget https://github.com/rochanlavu10/mcinstall123/raw/refs/heads/main/file123.zip
+unzip file123.zip
 mkdir .minecraft/home
-mkdir .minecraft/home/'Fabric 1.21.11'
-mv mods/ .minecraft/home/'Fabric 1.21.11'/mods
-xmessage "Optimization mods for Minecraft 1.21.11 Fabric have been installed!"
+mv file123/ .minecraft/home/'Fabric 1.21.11'
+xmessage "Optimization modpack for Minecraft 1.21.11 Fabric have been installed!"
